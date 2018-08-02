0a6f2a22f2a1cde9ac07a2633a03546cd91604df
Vaibhav Yadav <vaibhavy131@gmail.com>
2018-08-02 22:54:44 +0530

    created clock

---
+++ clock/main.js
@@ -0,0 +1,30 @@
+function Main() {
+  const secondHand = document.querySelector(".second-hand");
+  const minuteHand = document.querySelector(".minute-hand");
+  const hourHand = document.querySelector(".hour-hand");
+
+  function Clock() {
+    const d = new Date();
+    const seconds = d.getSeconds();
+    const secondDegree = (seconds / 60) * 360 + 90;
+    secondHand.style.transform = `rotate(${secondDegree}deg)`;
+
+    const minutes = d.getMinutes();
+    const minuteDegree = (minutes / 60) * 360 + 90;
+    minuteHand.style.transform = `rotate(${minuteDegree}deg)`;
+
+    const hours = d.getHours();
+    const hourDegree = (hours / 12) * 360 + 90;
+    hourHand.style.transform = `rotate(${hourDegree}deg)`;
+
+    if (seconds === 0) {
+      secondHand.style.transitionDuration = "0s";
+      minuteHand.style.transitionDuration = "0s";
+      hourHand.style.transitionDuration = "0s";
+    }
+  }
+
+  setInterval(Clock, 1000);
+}
+
+window.addEventListener("load", Main);
